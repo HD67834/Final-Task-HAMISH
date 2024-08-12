302fb099f44528a9c613f217f09cd4aabe5229a2
HD67834 <134247217+HD67834@users.noreply.github.com>
2024-08-12 12:45:45 +0800

    Add files via upload

---
+++ Merchandising.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Merchandising</title>
+    <link rel = "stylesheet" href="merchandising.css">
+</head>
+<body>
+    <?php echo "Hello World" ?>
+</body>
+</html>
